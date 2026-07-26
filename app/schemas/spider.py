350e8db62f9d5f6471453422735c1d5eabc33c28
Marcus Cevolela <marcus.cevolela@gmail.com>
2026-07-26 16:31:36 -0300

    feat: adiciona validações aos schemas

--- app/schemas/spider.py
+++ app/schemas/spider.py
@@ -1,18 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, HttpUrl
 
-class Spider(BaseModel):
+class SpiderBase(BaseModel):
+    name: str = Field(min_length=1)
+    secretIdentity: str = Field(min_length=1)
+    description: str = Field(min_length=1)
+    thumbnail: HttpUrl
+    banner: HttpUrl
+    slug: str = Field(min_length=1, pattern="^[a-z0-9-]+$")
+
+class SpiderCreate(SpiderBase):
+    pass
+
+class Spider(SpiderBase):
     id: int
-    name: str
-    secretIdentity: str
-    description: str
-    thumbnail: str
-    banner: str
-    slug: str
-
-class SpiderCreate(BaseModel):
-    name: str
-    secretIdentity: str
-    description: str
-    thumbnail: str
-    banner: str
-    slug: str
